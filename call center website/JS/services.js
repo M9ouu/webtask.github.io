04d5ababaa0bb4ab4d37797cc526705d958da200
M9ouu <patelmohmed986@gmail.com>
2022-08-15 12:26:13 +0530

    Call center website

---
+++ call center website/JS/services.js
@@ -0,0 +1,43 @@
+let topHeader = document.querySelector(".header-scroll");
+let hover = document.querySelector("#hover");
+let menuBtn = document.querySelector(".menu-button");
+console.log(hover);
+window.addEventListener("scroll", () => {
+  let scrolling = document.documentElement.scrollTop;
+  console.log();
+  if (scrolling >= 100) {
+    topHeader.classList.add("scroll");
+    hover.style.color = "black";
+    hover1.style.color = "black";
+    hover2.style.color = "black";
+    hover3.style.color = "black";
+    hover4.style.color = "black";
+    hover5.style.color = "black";
+    hover6.style.color = "black";
+    hover7.style.color = "black";
+    menuBtn.style.color = "black";
+  } else {
+    topHeader.classList.remove("scroll");
+    hover.style.color = "white";
+    hover1.style.color = "white";
+    hover2.style.color = "white";
+    hover3.style.color = "white";
+    hover4.style.color = "white";
+    hover5.style.color = "white";
+    hover6.style.color = "white";
+    hover7.style.color = "white";
+    menuBtn.style.color = "white";
+  }
+});
+function switchMode() {
+  let moon = document.getElementById("moon");
+  if (moon.className == "moon") {
+    moon.className = "sun";
+    document.body.style.backgroundColor = "#161e4a";
+    document.body.style.color = "#fff";
+  } else {
+    moon.className = "moon";
+    document.body.style.backgroundColor = "#fff";
+    document.body.style.color = "#000";
+  }
+}
